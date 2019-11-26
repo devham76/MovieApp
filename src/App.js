ba097eee346a38c9b76cb40158c2f3ca000facb3
hyemi <devham76@gmail.com>
2019-11-26 17:17:55 +0900

    proptypes

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import Movie from './Movie';
 import './App.css';
 
-const moviesTitles = [
-  "Harry Poter",
-  "Toy Story",
-  "Aladin"
-]
-const movieImages = [
-  "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5xq2/image/w2gbbJ7lwG0quKMZtTihufPuvno.jpg",
-  "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5uYQ/image/R3t_QxnFFfr20i8ekqVdzxUnBYE.jpg",
-  "http://img.movist.com/?img=/x00/05/11/49_p1.jpg"
+const movies = [
+  {
+    title : "Harry Poter",
+    poster : "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5xq2/image/w2gbbJ7lwG0quKMZtTihufPuvno.jpg"
+  },
+  {
+    title : "Toy Story",
+    poster : "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5uYQ/image/R3t_QxnFFfr20i8ekqVdzxUnBYE.jpg"
+  },
+  {
+    title : "Aladin",
+    poster :   "http://img.movist.com/?img=/x00/05/11/49_p1.jpg"
+  }
 ]
 
 function App() {
+  componentWillMonut(){
+    console.log(" will mount");
+  }
+  componentDidMount(){
+    console.log("did mount");
+  }
   return (
     <div className="App">
-     <Movie title={moviesTitles[0]} poster={movieImages[0]} />
-     <Movie title={moviesTitles[1]} poster={movieImages[1]} />
-     <Movie title={moviesTitles[2]} poster={movieImages[2]} /> 
+      {movies.map( (movie, index) => {
+        return <Movie title={movie.title} poster={movie.poster} key={index} />
+      })}
     </div>
   );
 }
