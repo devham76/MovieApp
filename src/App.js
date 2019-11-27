170bc8914040b2f600c21e322983483b4a80fbf0
hyemi <devham76@gmail.com>
2019-11-27 21:10:30 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -12,32 +12,33 @@ class App extends React.Component {
   }
 
   // 비동기화 함수
-  _getMovies = async() => {
+  _getMovies = async () => {
     // await를 하는 것은 call api기능이 끝나는것을 기다리고
     // 끝나면 그 결과값을 movies에 넣는다
     const movies = await this._callApi()
     // 해당부분은 callApi 작업이 완료 되기 전까지는 실행되지 않는다
+    console.log(movies)
     this.setState({
-       // 모던 자바스크립트의 표현, 이전표현 => movies : movies
-      movies 
+      // 모던 자바스크립트의 표현, 이전표현 => movies : movies
+      movies
     })
   }
 
   //-- url 을 ajax로 불러온다
-  _callApi = () =>{
-    const url = "https://yts.lt/api/v2/list_movies.json?sort_by=rating"; 
+  _callApi = () => {
+    const url = "https://yts.lt/api/v2/list_movies.json?sort_by=rating";
     return fetch(url)
-    .then( potato => potato.json()) // json으로 변경
-    .then( json => json.data.movies)
-    .catch( err => console.log(err))
+      .then(potato => potato.json()) // json으로 변경
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
   }
 
   // 리액트는 자체 기능이 많기 때문에
   // 리액트 자체기능과 나의 기능에 차이를 두려고 '_'를 사용한다
   _renderMovies = () => {
     // movies라는 변수에 데이터 저장
-    const movies = this.state.movies.map( movie => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title_english} poster={movie.medium_cover_image} genres={movie.genres} synopsis={movie.synopsis} rating={movie.rating} key={movie.id} />
     })
     return movies
   }
@@ -47,7 +48,8 @@ class App extends React.Component {
   // 해결 방법 -> movies가 없으면 "Loading"출력, 있으면 movies출력
   render() {
     return (
-      <div className="App">
+      // state에 movie가 있는지 확인한다.
+      <div className={this.state.movies ? "App" : "App--loading"}>
         {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,28 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 import './Movie.css';
+import generate from '@babel/generator';
+import LinesEllipsis from 'react-lines-ellipsis'
 
+
+function Movie({ title, poster, genres, synopsis, rating }) {
+  return (
+    <div className="Movie">
+      <div className="Movie__Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie__Columns">
+        <h1>{title}</h1>
+        <div className="Moive__Genres">
+          {genres.map((genre, index) => {
+            return <MovieGenre genre={genre} key={index} />
+          })}
+        </div>
+        <div className="Moive__Rating">
+          {rating}
+        </div>
+        <p className="Movie__Synopsis">
+
+          <LinesEllipsis
+            text={synopsis}
+            maxLine='3'
+            ellipsis=' ... '
+            trimRight
+            basedOn='letters'
+          />
+        </p>
+      </div>
+    </div>
+  )
+}
+
+function MoviePoster({ poster, alt }) {
+  return (
+    <img className="Movie__Poster" src={poster} alt={alt} />
+  )
+}
+
+function MovieGenre({ genre }) {
+  return (
+    <span className="Movie__Genre">{genre} </span>
+  )
+}
 
 Movie.proptype = {
   title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
-}
-function Movie({ title, poster }) {
-  return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
-    </div>
-  )
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.string.isRequired,
+  synopsis: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 MoviePoster.prototype = {
   poster: PropTypes.string.isRequired
 }
-function MoviePoster({ poster }) {
-  return (
-    <img className="posterImg" src={poster} alt="poster" />
-  )
-}
 
+MovieGenre.prototype = {
+  genres: PropTypes.string.isRequired
+}
 export default Movie;
